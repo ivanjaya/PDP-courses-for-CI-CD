43cdd48e560679562c9c250cfd740868fe230f55
Rionardo <rio@fivejack.com>
2022-05-27 06:01:59 +0000

    [skip ci] Updates

---
+++ _next/static/PtKAhbuVniSLEovoSZvtQ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-03c8959f893bc2ff.js"],"/_error":["static/chunks/pages/_error-635a6533d6ac3ca1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
